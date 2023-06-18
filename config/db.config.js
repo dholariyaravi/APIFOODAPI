0c0af9f04d068a922ad577962cfea5ece2881529
dholariyaravi <ravidholariya700@gmail.com>
2023-06-18 16:05:23 +0530

    swss

--- config/db.config.js
+++ config/db.config.js
@@ -1,5 +1,5 @@
 //mongo DB create database(collection)
 module.exports = {
-    // url: "mongodb+srv://rkofficial:rkofficial@cluster0.jbk9za3.mongodb.net/FoodOrder";
-        url: "mongodb+srv://ravidholariya700:<password>@cluster0.kfr5fqp.mongodb.net/"
+    // url: "mongodb+srv://rkofficial:rkofficial@cluster0.jbk9za3.mongodb.net/FoodOrder"
+     url: "mongodb+srv://ravidholariya700:ravidholariya@cluster0.kfr5fqp.mongodb.net/"
 };
